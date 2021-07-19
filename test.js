ccbb2fd6b65a70c42430a732e00049630d13d1c5
Ganning Xu <ganningxu@gmail.com>
2021-07-19 12:34:40 -0400

    format

--- test.js
+++ test.js
@@ -12,24 +12,23 @@ const app = require("./src/http/post-api-github-webhooks/app");
 let probot;
 const test = suite("app");
 test.before.each(() => {
-    probot = new Probot({
-        // simple authentication as alternative to appId/privateKey
-        githubToken: "test",
-        // disable logs
-        logLevel: "warn",
-        // disable request throttling and retries
-        Octokit: ProbotOctokit.defaults({
-            throttle: { enabled: false },
-            retry: { enabled: false },
-        }),
-    });
-    probot.load(app);
+  probot = new Probot({
+    // simple authentication as alternative to appId/privateKey
+    githubToken: "test",
+    // disable logs
+    logLevel: "warn",
+    // disable request throttling and retries
+    Octokit: ProbotOctokit.defaults({
+      throttle: { enabled: false },
+      retry: { enabled: false },
+    }),
+  });
+  probot.load(app);
 });
 
 test("recieves issues.opened event", async function () {
-
-    console.log("TRUE");
-    return true;
+  console.log("TRUE");
+  return true;
 });
 
 test.run();
