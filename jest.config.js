a9e4e406ae413ba17c3cf72370225a818637f6d1
Ganning Xu <ganningxu@gmail.com>
2021-07-19 11:06:03 -0400

    passwnotest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    preset: 'ts-jest',
+    moduleFileExtensions: ['ts', 'tsx', 'js'],
+    globals: { 'ts-jest': { tsConfig: 'tsconfig.json' } },
+    testMatch: ['**/test/**/*.test.(ts|tsx)'],
+    passWithNoTests: true
+  };
