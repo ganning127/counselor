163ea60967df394293885151c49658e1c2a81725
Ganning Xu <ganningxu@gmail.com>
2021-07-19 11:07:41 -0400

    no tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-    preset: 'ts-jest',
     moduleFileExtensions: ['ts', 'tsx', 'js'],
-    globals: { 'ts-jest': { tsConfig: 'tsconfig.json' } },
-    testMatch: ['**/test/**/*.test.(ts|tsx)'],
-    passWithNoTests: true
+    testMatch: [],
   };
